0c976f08c3eb378e12cd1d47899c25f0bca23c24
Chester Keh <114325356+BuffBulbasaur@users.noreply.github.com>
2023-11-28 16:06:32 +0800

    fixed ship placement

--- script.js
+++ script.js
@@ -1,27 +1,57 @@
 //* Variables //
-let boardSize = 3; // preset to 3 first at the start.
+let boardSize = 5; // preset to 3 first at the start.
 let playerTurn = true; // to keep track of whose turn it is
+let computerTurn = false;
 
 const playerBoard = document.getElementById("playerBoard");
 const computerBoard = document.getElementById("computerBoard");
 
-// Function to generate random ship positions
-function generateRandomShipPositions() {
-  const positions = [];
-  for (let i = 0; i < 3; i++) {
-    // For simplicity, assuming ships are of size 1x1
-    const row = Math.floor(Math.random() * boardSize);
-    const col = Math.floor(Math.random() * boardSize);
-    positions.push({ row, col });
-  }
-  return positions;
-}
+const smallShips = document.getElementById("smlShip");
+const mediumShip = document.getElementById("midShip");
+const theBigShip = document.getElementById("bigShip");
 
-// Generate random ship positions for player and computer
-const playerShipPositions = generateRandomShipPositions();
-const computerShipPositions = generateRandomShipPositions();
+//* Variables //
+
+//* ship placement first [fixed] *//
+const smlShips = [
+  { row: 1, col: 2 },
+  { row: 3, col: 0 },
+];
+
+const midShips = [
+  { row: 0, col: 1 },
+  { row: 0, col: 2 },
+  { row: 0, col: 3 },
+  { row: 2, col: 2 },
+  { row: 2, col: 3 },
+  { row: 2, col: 4 },
+];
+
+const bigShips = [
+  { row: 4, col: 0 },
+  { row: 4, col: 1 },
+  { row: 4, col: 2 },
+  { row: 4, col: 3 },
+  { row: 4, col: 4 },
+];
+
+//* ship placement first [fixed] *//
+
+//! Function to generate random ship positions //
+// function generateRandomShipPositions() {
+//   const positions = [];
+//   for (let i = 0; i < 3; i++) {
+//     // For simplicity, assuming ships are of size 1x1
+//     const row = Math.floor(Math.random() * boardSize);
+//     const col = Math.floor(Math.random() * boardSize);
+//     positions.push({ row, col });
+//   }
+//   return positions;
+// }
+//! Function to generate random ship positions //
 
 // Function to draw the grid
+//referance from https://jsfiddle.net/pf4gbm4L/
 function drawGrid(cells, container, shipPositions) {
   container.innerHTML = "";
 
@@ -38,13 +68,29 @@ function drawGrid(cells, container, shipPositions) {
       cell.style.height = cellSize + "px";
       cell.appendChild(document.createTextNode("\u00A0"));
 
-      // Add event listener for each cell to handle clicks
-      cell.addEventListener("click", function () {
-        if (playerTurn) {
-          handlePlayerClick(this, shipPositions);
-        }
-        // You can add computer's turn logic here if needed
-      });
+      //Check if current cell is a ship
+      const isSmlShipPosition = smlShips.some(
+        (pos) => pos.row === i && pos.col === j
+      );
+
+      const isMidShipPosition = midShips.some(
+        (pos) => pos.row === i && pos.col === j
+      );
+
+      const isBigShipPosition = bigShips.some(
+        (pos) => pos.row === i && pos.col === j
+      );
+
+      if (isSmlShipPosition) {
+        cell.classList.add("smlShip");
+      }
+
+      if (isMidShipPosition) {
+        cell.classList.add("midShip");
+      }
+      if (isBigShipPosition) {
+        cell.classList.add("bigShip");
+      }
 
       row.appendChild(cell);
     }
@@ -52,61 +98,18 @@ function drawGrid(cells, container, shipPositions) {
   }
 }
 
-// Function to handle player clicks
-function handlePlayerClick(cell, shipPositions) {
-  const cellIndex = Array.from(cell.parentNode.children).indexOf(cell);
-
-  // Check if the clicked cell is part of a ship
-  const isHit = isCellPartOfShip(cellIndex, shipPositions);
-
-  // Add visual cues for hits and misses
-  if (isHit) {
-    cell.classList.add("hit");
-  } else {
-    cell.classList.add("miss");
-  }
-
-  // Switch to the computer's turn
-  playerTurn = false;
-  setTimeout(handleComputerTurn, 1000);
-}
-function handleComputerTurn() {
-  const randomCellIndex = Math.floor(Math.random() * (boardSize * boardSize));
-  const randomCell = playerBoard.children[randomCellIndex];
-
-  const isHit = isCellPartOfShip(randomCellIndex, playerShipPositions);
-  if (isHit) {
-    randomCell.classList.add("hit");
-  } else {
-    randomCell.classList.add("miss");
-  }
-  playerTurn = true;
-}
-
-// Function to check if the clicked cell is part of a ship
-function isCellPartOfShip(cellIndex, shipPositions) {
-  const cellRow = Math.floor(cellIndex / boardSize);
-  const cellCol = cellIndex % boardSize;
-
-  return shipPositions.some(
-    (position) => position.row === cellRow && position.col === cellCol
-  );
-}
-
 // Event listener for grid size change
 document.getElementById("selectGrid").addEventListener("change", function () {
   boardSize = parseInt(this.value, 10);
 
   // Generate new random ship positions for player and computer
-  const newPlayerShipPositions = generateRandomShipPositions();
-  const newComputerShipPositions = generateRandomShipPositions();
 
-  drawGrid(boardSize, playerBoard, newPlayerShipPositions);
-  drawGrid(boardSize, computerBoard, newComputerShipPositions);
+  drawGrid(boardSize, playerBoard, smlShips, midShips, bigShips);
+  drawGrid(boardSize, computerBoard, smlShips, midShips, bigShips);
 
   playerTurn = true;
 });
 
 // Initial draw
-drawGrid(boardSize, playerBoard, playerShipPositions);
-drawGrid(boardSize, computerBoard, computerShipPositions);
+drawGrid(boardSize, playerBoard, smlShips, midShips, bigShips);
+drawGrid(boardSize, computerBoard, smlShips, midShips, bigShips);
